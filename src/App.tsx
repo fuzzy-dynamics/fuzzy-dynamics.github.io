5b0c7706ba3843c7b77cea12b95fda7019d56b4f
banrovegrie <ac.ala.arya@gmail.com>
2025-08-04 17:48:49 +0530

    Deploy React app to GitHub Pages - fix empty page issue

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/blog" element={<Blog />} />
